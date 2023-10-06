7a2b16918158b2077a2724e604b55ec548b9cf3b
Sambhavi PD <sambhavi@golittlebig.com>
2023-10-06 18:44:57 +0530

    created a book app

--- mybookshelf/mybookshelf/settings/base.py
+++ mybookshelf/mybookshelf/settings/base.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "book",
 ]
 
 MIDDLEWARE = [
